b4d8662d56457d1ed9447fc4490755e17ba6214f
nam <namttdh@gmail.com>
2019-10-06 01:44:30 +0700

    ađ favorite icon and add form contact function

--- js/agency.js
+++ js/agency.js
@@ -40,3 +40,19 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+$.fn.serializeObject = function() {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function() {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+};
